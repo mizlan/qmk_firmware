f9464e5df0529bea3e44874968c9562f03e1d280
Michael Lan <michaellan202@gmail.com>
2025-05-27 11:51:56 -0700

    Add mizlan keymap for TKD Cycle7

--- keyboards/vertex/cycle7/config.h
+++ keyboards/vertex/cycle7/config.h
@@ -17,3 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #define WS2812_SPI_DRIVER SPID2
 #define WS2812_SPI_MOSI_PAL_MODE 5
+
+/*
+ * Another key-down on a Tap-or-Hold key is immediately emitted as a
+ * hold upon the simultaneous key-down of another key.
+ *
+ * ref: https://docs.qmk.fm/tap_hold#hold-on-other-key-press
+ */
+#define HOLD_ON_OTHER_KEY_PRESS
+
+/*
+ * By default, if a Tap-or-Hold key is tapped and then immediately pressed
+ * down again, the latter emits as a tap (to allow a way for hold-for-key-repeat).
+ * Prefer emitting hold.
+ *
+ * ref: https://docs.qmk.fm/tap_hold#quick-tap-term
+ */
+#define QUICK_TAP_TERM 0
